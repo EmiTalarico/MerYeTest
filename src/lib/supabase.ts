f72c9696b8d88334830f8f44ccdee6791298c061
Emilio Talarico <emilio.talarico@hotmail.com>
2026-04-04 22:47:28 -0300

    variables de entorno configuradas

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://xsltpssytriidsvqdake.supabase.co";
-const supabaseKey = "sb_publishable_JxL6JFgs3-GtIqkkaXGKyA_sY4N_EYF";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
